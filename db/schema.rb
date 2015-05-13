330dc605245ebd692d046bf7139c5f9a5a10ae92
Andrew Weist <andrew.weist@gmail.com>
2015-05-13 04:07:03 -0400

    New User attributes

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150512023957) do
+ActiveRecord::Schema.define(version: 20150512232717) do
 
   create_table "users", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email_address"
   end
 
 end
