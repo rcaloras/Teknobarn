cfc6296d87f41ba2743bd18dfa5797fea2251722
Andrew Weist <aweist@gilt.com>
2015-07-10 01:28:19 -0400

    fixing hero images

--- db/migrate/20150625093825_create_users.rb
+++ db/migrate/20150625093825_create_users.rb
@@ -1,6 +1,6 @@
 class CreateUsers < ActiveRecord::Migration
   def change
-    drop_table(:users, if_exists: true)
+    drop_table :users if ActiveRecord::Base.connection.table_exists? :users
     create_table :users do |t|
       t.string :first_name
       t.string :last_name
