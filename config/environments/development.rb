e85ab7ff913f2f7a3da3417d4a14a59a46744e65
Andrew Weist <aweist@gilt.com>
2015-07-10 18:14:13 -0400

    Trying to fix mail server

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,21 +39,27 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-
-  config.action_mailer.delivery_method = :smtp
-  # SMTP settings for gmail
-  config.action_mailer.smtp_settings = {
-   :address              => "smtp.gmail.com",
-   :port                 => 587,
-   :user_name            => ENV["GMAILUSER"],
-   :password             => ENV["GMAILPASS"],
-   :authentication       => "plain",
-   :enable_starttls_auto => true
-  }
-
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_options = {
-   :reply_to             => "andrew.weist@gmail.com"
+    :from                 => "Teknobarn <noreply@teknobarn.com>",
+    :reply_to             => "andrew.weist@gmail.com"
   }
+
+  # config.action_mailer.delivery_method = :smtp
+  # # SMTP settings for gmail
+  # config.action_mailer.smtp_settings = {
+  #  :address              => "smtp.gmail.com",
+  #  :port                 => 587,
+  #  :user_name            => ENV["GMAILUSER"],
+  #  :password             => ENV["GMAILPASS"],
+  #  :authentication       => "plain",
+  #  :enable_starttls_auto => true
+  # }
+
+  # config.action_mailer.default_options = {
+  #  :reply_to             => "andrew.weist@gmail.com"
+  # }
 
   config.action_mailer.perform_deliveries = true
 
