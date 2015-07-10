547a250a4067e2a578334409e9a69a1d41ac29c1
Andrew Weist <aweist@gilt.com>
2015-07-10 12:14:33 -0400

    fix text version of mailer

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,6 +1,7 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
   def invite
+    User.create(:first_name => 'Andrew', :last_name => 'Weist', :email => 'andrew.weist@gmail.com')
     UserMailer.invite_email(User.first)
   end
 
