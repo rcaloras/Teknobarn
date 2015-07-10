f0f6958d2873d6720c66a9507184ad5428f4d7c3
Andrew Weist <aweist@gilt.com>
2015-07-10 18:58:26 -0400

    Trying to fix from email name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,8 +85,8 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_options = {
-    :from                 => "Teknobarn <noreply@teknobarn.com>",
-    :reply_to             => "andrew.weist@gmail.com"
+    :from                 => '"Teknobarn" <noreply@teknobarn.com>',
+    :reply_to             => 'andrew.weist@gmail.com'
   }
 
   # config.action_mailer.delivery_method = :smtp
