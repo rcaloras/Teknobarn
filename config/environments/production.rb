32319818b350b917f3495e3c7cb58e336b6f6ee7
Andrew Weist <aweist@gilt.com>
2015-07-10 18:00:31 -0400

    Change from email name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_options = {
-    :from                 => 'noreply@teknobarn.com',
+    :from                 => 'Teknobarn <noreply@teknobarn.com>',
     :reply_to             => 'andrew.weist@gmail.com'
   }
 
