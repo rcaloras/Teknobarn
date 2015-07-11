fa83fa6b4b9e05b284288b2d522c004fd8f58426
Andrew Weist <andrew.weist@gmail.com>
2015-07-11 17:23:03 -0400

    Remove credentials from file

--- config/initializers/amazon_ses.rb
+++ config/initializers/amazon_ses.rb
@@ -1,4 +1,4 @@
 ActionMailer::Base.add_delivery_method :ses, AWS::SES::Base,
   :server            => 'email.us-west-2.amazonaws.com',
-  :access_key_id     => 'AKIAJDJPBTXPSJN65AIQ',
-  :secret_access_key => '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'
+  :access_key_id     => ENV['SES_USER'],
+  :secret_access_key => ENV['SES_PASS']
